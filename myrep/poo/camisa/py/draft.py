373ff9151602ac30ebe0f957cbfa1b873d0ba1b2
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-10-15 01:12:01 +0000

    camisa

--- myrep/poo/camisa/py/draft.py
+++ myrep/poo/camisa/py/draft.py
@@ -8,13 +8,17 @@ class Camisa:
     def setTamanho(self, valor: str):
         if valor != "P" and valor != "PP" and valor != "M" and valor != "G" and valor != "GG" and valor != "XG":
             print("Valores permitidos: PP, P, M, G, GG, XG.")
+        else:
+            self.__tamanho = valor
+
 
 # loop principal
-roupa = Roupa() # criando roupa com valor tamanho padrão
-
+roupa = Camisa() # crian1do roupa com valor tamanho padrão
 while roupa.getTamanho() == "": # mantendo usuário no loop
     print("Digite seu tamanho de roupa")
-    tamanho = input() # lendo a resposta
+    tamanho: str = input()
+        print("$" + line)
+        args: list[str] = tamanho.split(" ")
     roupa.setTamanho(tamanho) # tentando atribuir e disparando erros
 
 print("Parabens, você comprou uma roupa tamanho", roupa.getTamanho())
