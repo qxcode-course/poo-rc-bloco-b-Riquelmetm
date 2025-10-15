1b5d0f45301cef869d184e19affd9eb678e75638
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-10-15 02:57:49 +0000

    chinela final

--- myrep/poo/chinela/py/draft.py
+++ myrep/poo/chinela/py/draft.py
@@ -1,5 +1,5 @@
 class Chinela:
-    def __init__(self)
+    def __init__(self):
         self.__tamanho: int = 0
     def getTamanho (self):
         return self.__tamanho
@@ -8,12 +8,13 @@ class Chinela:
             if valor%2 == 1:
                 print("A chinela é de tamanho: ",valor,":",valor + 1)
                 self.__tamanho = valor
-                break
             else:
                 if valor == 20:
                     print("A chinela é do tamanho", valor)
-                print("A chinela é de tamanho: "valor - 1 ":"valor)
-                break
+                    self.__tamanho = valor
+                else:
+                    print("A chinela é de tamanho: ",valor - 1 ,":",valor)
+                    self.__tamanho = valor
         else:
             print("Valor invalido, digite novamente:")
 chinelo = Chinela()
@@ -21,4 +22,4 @@ while chinelo.getTamanho() == 0:
     print("Digite o tamanho da chinela: ")
     valor = int(input())
     chinelo.setTamanho(valor)
-print("Parabens, você comprou uma chinela tamanho", chinela.getTamanho())
+print("Parabens, você comprou uma chinela tamanho", chinelo.getTamanho())
