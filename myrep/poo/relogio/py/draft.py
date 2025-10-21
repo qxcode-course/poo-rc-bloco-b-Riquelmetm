dba5c0a96867a59daf5f0e9f170542e2e6a56e0d
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-10-21 02:03:19 +0000

    relogio

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -1,34 +1,38 @@
 class Watch:
     def __init__(self, hora:int, minuto:int, segundo:int):
         self.__hora = 0
+        self.setHora(hora)
         self.__minuto = 0
+        self.setMinuto(minuto)
         self.__segundo = 0
+        self.setSegundo(segundo)
+
+    def __str__(self):
+        return f"{self.__hora}:{self.__minuto}:{self.__segundo}"
 
     def getHora(self):
         return self.__hora
+
     def setHora(self, hora: int):
         if hora < 0 or hora > 23:
             print("fail: hora invalida")
             return
         self.__hora = hora
 
-    def setminuto (self, minuto:int):
+    def setMinuto (self, minuto:int):
         if minuto > 59 or minuto < 0:
             print("fail: minuto invalida")
             return
         self.__minuto = minuto
+
+    def setSegundo (self, segundo:int)
+        if segundo < 0 or segundo > 59:
+            print("fail: segundo invalido")
+            return
+        self.__segundo = segundo
+
+    
+
         
-    def setsegundo (self, segundo:int)
 
-    def __str__(self):
-        return f"{self.__hora}:{self.__minuto}:{self.__segundo}"
-        
-
-relogio = Watch(111,0,0)
-
-relogio.__hora = 89797
-print(relogio)
-relogio.setHora(6)
-# print(relogio.getHora())
-
-print(relogio)
+relogio = Watch()
