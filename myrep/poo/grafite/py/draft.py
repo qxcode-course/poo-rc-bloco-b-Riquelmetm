f15e8fe884888608e486c781215895132a1c05b0
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-10-28 05:01:09 +0000

    grafite

--- myrep/poo/grafite/py/draft.py
+++ myrep/poo/grafite/py/draft.py
@@ -1,2 +1,19 @@
 class Grafite:
     def __init__ (self, calibre: float, dureza: str, tamanho: int):
+        self.calibre = calibre
+        self.dureza = dureza
+        self.tamanho = tamanho
+    def GastoPorFolha (self):
+        if self.dureza == "HB":
+            return 1
+        elif self.dureza == "2B":
+            return 2
+        elif self.dureza == "4B":
+            return 4
+        elif self.dureza == "6B":
+            return 6
+
+class Lapiseira:
+    def __init__(self, calibre: float):
+        self.calibre = calibre
+        self.ponta = None
